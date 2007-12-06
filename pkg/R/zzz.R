fc25c2f0d2df181cd95d04881a37ef48276e78ec
skli (Søren Klim) <skl@imm.dtu.dk>
2007-12-06 10:23:13 +0000

    Matrix Exponential - matexp has been added to project
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/psm@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,5 +1,8 @@
 .First.lib <- function(lib, pkg) {
-    loaded <- suppressWarnings(require(mexp))
+
+  library.dynam("PSM", pkg, lib)
+  
+  loaded <- suppressWarnings(require(mexp))
     if(!loaded) {
      	print("Searching for package in lib.loc")
     	require(mexp,lib.loc=lib)
