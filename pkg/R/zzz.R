a49c7bcc26b8b818daf18bb79db861d8c0320cbc
stigbm (Stig B. Mortensen) <sbm@imm.dtu.dk>
2007-12-06 15:04:57 +0000

    mexp-pakke fjernet
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/psm@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,11 +1,4 @@
 .First.lib <- function(lib, pkg) {
 
   library.dynam("PSM", pkg, lib)
-  
-  loaded <- suppressWarnings(require(mexp))
-    if(!loaded) {
-     	print("Searching for package in lib.loc")
-    	require(mexp,lib.loc=lib)
-    	}
-    
 }
