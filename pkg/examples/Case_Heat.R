e396092018c287c875f56339b6d014e70aa59be8
stigbm (Stig B. Mortensen) <sbm@imm.dtu.dk>
2008-03-27 08:07:51 +0000

    Update of Case_Heat and inclusion of test with NAs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/psm@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/examples/Case_Heat.R
+++ pkg/examples/Case_Heat.R
@@ -7,6 +7,9 @@ rm(list=ls())
 detach(package:PSM)
 
 library(PSM,lib.loc="~/PSM/Rpackages/gridterm")
+source("../R/PSM.estimate.R",echo=F)
+source("../R/invlogit.R",echo=F)
+source("../R/LinKalmanFilter.R",echo=F)
 
 # Load the Data and Variables
 tmpData <- read.table("Heat_Data.csv",sep=";", col.names=c("TIME","Te","Ti","Q"))
@@ -55,13 +58,13 @@ names(HeatModel)
 # Initial guess from CTSM
 # THETA OBJ             G1,   G2,  H1,  H2,  H3, SIG11,SIG22,    S, X01,  X02
 # CTSM starting guess fails in this implementation
-par1 <- list(LB   = c(   0,    0,   0,   0,   0,     0,    0,    0,   10,   20),
+par1 <- list(LB   = c(  10,   10,1e-1,1e-1,1e-2,  1e-8, 1e-8, 1e-4,   10,   20),
              Init = c( 100,   50,   1,   2,  .5,   .01,  .01,  .01,   15,   25),
              UB   = c( 200,  100,   2,   5,   1,     1,    1,    1,   20,   30)
              )
-
-par1$Init <-        c( 100 ,  50,   1,   2,  .5,  .001, .001, .001,   13,   25)
-par1$UB <- par1$LB <- NULL
+APL.KF(par1$Init,HeatModel,Pop.Data)
+#par1$Init <-        c( 100 ,  50,   1,   2,  .5,  .001, .001, .001,   13,   25)
+#par1$UB <- par1$LB <- NULL
 
 
 # Check the Model
@@ -75,7 +78,11 @@ ModelCheck( Model=HeatModel , Data=Pop.Data[[1]], Par=par1)
 # CTSM returns -LL= -623 
 CTSMphi <- c( 1.3134E+01,2.5330E+01,1.0394E+02,9.6509E-01,2.0215E+00,4.9320E+01,5.0929E-01,7.3779E-08,2.6951E-09,1.0330E-02)
 names(CTSMphi) <- c("X01","X02","G1","H1","H2","G2","H3","SIG11","SIG22","S")
-Ob1 <- LinKalmanFilter( phi=CTSMphi , Model=HeatModel , Data=Pop.Data[[1]] , echo=T, outputInternals=TRUE)
+CTSMTHETA=c(CTSMphi[["G1"]],   CTSMphi[["G2"]],  CTSMphi[["H1"]],  CTSMphi[["H2"]],  CTSMphi[["H3"]], CTSMphi[["SIG11"]],CTSMphi[["SIG22"]],    CTSMphi[["S"]], CTSMphi[["X01"]],  CTSMphi[["X02"]])
+Ob1 <- LinKalmanFilter( phi=CTSMphi , Model=HeatModel , Data=Pop.Data[[1]] , echo=FALSE, outputInternals=TRUE,fast=FALSE)
+Ob1$negLogL #[1,] -623.3564 in R
+APL.KF(CTSMTHETA,HeatModel,Pop.Data)
+
 
 # Validation plot versus Data
 D <- Pop.Data[[1]]
@@ -86,9 +93,6 @@ points( D$Time , Ob1$Yp, pch="+",col="blue")
 # Minimizers
 # -------------------------------------------------------------
                                         # Test Run of initial parameters
-phi <- par1
-names(phi$Init) <- c("G1","G2","H1","H2","H3","SIG11","SIG22","S","X01","X02")
-phi$Init
 
 # Perform minimization with 2 different optimizers
 #Rprof()
@@ -106,11 +110,12 @@ cat( "optim: " , Min2$sec, "\t ", Min2$opt$value, "\n")
 # Smoother 
 # -------------------------------------------------------------
 
-SmoothObj <- PSM.smooth(Model=HeatModel, Data=Pop.Data, THETA=CTSMphi, subsample=0,trace=1)
+SmoothObj <- PSM.smooth(Model=HeatModel, Data=Pop.Data, THETA=CTSMTHETA, subsample=0,trace=1)
 
 
 D <- SmoothObj[[1]]
 names(D)
+D$negLogL
 
 Idx <- 200:500
 plot( D$Time[Idx], D$Xs[1,Idx] , type="n" )
@@ -130,6 +135,7 @@ plot(Pop.Data[[1]]$Time, Pop.Data[[1]]$Y  )
 lines( CTSM.Val.Data$Time, CTSM.Val.Data$Y1, col="red")
 Idx <- !is.na(D$Yp)
 lines( D$Time[Idx] , D$Yp[Idx] , col="blue")
+plot(CTSM.Val.Data$Y1[Idx]/D$Yp[Idx])
 
 # State validation
 plot(D$Time[-(1:10)], D$Xp[1,-(1:10)],type="l",col="red")
@@ -139,3 +145,40 @@ plot(CTSM.Val.Data$Time , CTSM.Val.Data$Xp2,type="l",col="blue",ylim=range(CTSM.
 lines(CTSM.Val.Data$Time , CTSM.Val.Data$Xp1,type="l",col="blue")
 
 
+
+
+# -------------------------------------------------------------
+# Include NAs in data 
+# -------------------------------------------------------------
+
+
+Pop.DataNA <- Pop.Data
+Pop.DataNA[[1]]$Y <- matrix(NA,ncol=length(Pop.DataNA[[1]]$Time)*2,nrow=2)
+Pop.DataNA[[1]]$U <- matrix(NA,ncol=length(Pop.DataNA[[1]]$Time)*2,nrow=2)
+for(i in 1:length(Pop.DataNA[[1]]$Time)) {
+  Pop.DataNA[[1]]$Y[(i%%2)+1,i*2-1] <- Pop.Data[[1]]$Y[1,i]
+  Pop.DataNA[[1]]$U[,i*2-1] <- Pop.DataNA[[1]]$U[,i*2] <- Pop.Data[[1]]$U[,i]
+}
+Pop.DataNA[[1]]$Time <- seq(0,718.5,.5)
+
+# Show new Y, U and Time
+rbind(Pop.DataNA[[1]]$Y[,1:8],
+      Pop.DataNA[[1]]$U[,1:8],
+      Pop.DataNA[[1]]$Time[1:8])
+
+# Update model til 2-dim Y
+HeatModelNA <- HeatModel
+HeatModelNA$Matrices <- function(phi=NA) {
+  G1  <- phi[["G1"]] ; G2  <- phi[["G2"]]
+  H1  <- phi[["H1"]] ; H2  <- phi[["H2"]] ; H3  <- phi[["H3"]]
+  list(
+       matA = matrix( c(-1*(1/H1+1/H2)/G1,1/(G1*H2),1/(G2*H2) , -1*(1/H2+1/H3)/G2 ) , ncol=2, byrow=T),
+       matB = diag( c(1/(G1*H1) , 1/(G2*H3) ) ),
+       matC = matrix( rep(c(0,-1/H3),each=2) ,nrow=2),
+       matD = matrix( rep(c(0,1/H3),each=2)  ,nrow=2))
+}
+HeatModelNA$S <- function(phi=NA) {
+   diag(rep(phi[["S"]],2)) }
+
+ModelCheck( Model=HeatModelNA , Data=Pop.DataNA[[1]], Par=par1)
+APL.KF(CTSMTHETA,HeatModelNA,Pop.DataNA) #[1] -623.3564
